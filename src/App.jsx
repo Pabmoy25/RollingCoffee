512cdc47f2460701213273004c3e01bc228dba01
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-06 15:12:05 -0300

    Se realiza maquetado inicial de componente Inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
-import Footer from './components/Common/Footer'
-import Menu from './components/Common/Menu'
-import Inicio from './components/pages/Inicio'
-import Error404 from './components/pages/Error404';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Footer from "./components/Common/Footer";
+import Menu from "./components/Common/Menu";
+import Inicio from "./components/pages/Inicio";
+import Error404 from "./components/pages/Error404";
 
 function App() {
- 
   return (
     <>
-    <section className='mainpage'>
-      <Menu></Menu>
-     {/*  <Inicio></Inicio> */}
-      <Error404></Error404>
+      <section className="mainpage">
+        <Menu></Menu>
+        <Inicio></Inicio>
+        {/* <Error404></Error404> */}
       </section>
       <Footer></Footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,9 +1,47 @@
+import { Button, Card, Container, Image } from "react-bootstrap";
+import banner from "../../assets/banner.png";
+import CoffeeBanner from "../../assets/CoffeeBanner.png";
+
 const Inicio = () => {
-    return (
-        <div>
-<p>main de pagina principal</p>            
-        </div>
-    );
+  return (
+    <>
+      
+      <div>
+        <Image src={CoffeeBanner} className="img-fluid" />
+      </div>
+      
+      <h2 className="text-center mt-4">Nuestros Productos</h2>
+       
+    <Container className="d-flex">
+        
+      <Card className="m-4" style={{ width: "14rem" }}>
+        <Card.Img variant="top" src={banner} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="success">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      <Card className="m-4" style={{ width: "14rem" }}>
+        <Card.Img variant="top" src={banner} />
+        <Card.Body>
+          <Card.Title>Card Title</Card.Title>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+          <Button variant="success">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+
+      </Container>
+
+    </>
+  );
 };
 
 export default Inicio;
