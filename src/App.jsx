611159311f3f6ea04d1486d0af174f31435a43db
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-11 17:06:22 -0300

    Se creo la tabla del administrador

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Footer from "./components/Common/Footer";
 import Menu from "./components/Common/Menu";
 import Inicio from "./components/pages/Inicio";
 import Error404 from "./components/pages/Error404";
+import Administrador from "./components/pages/Administrador";
 
 function App() {
   return (
     <>
       <section className="mainpage">
         <Menu></Menu>
-        <Inicio></Inicio>
+        {/* <Inicio></Inicio> */}
         {/* <Error404></Error404> */}
+        <Administrador></Administrador>
       </section>
       <Footer></Footer>
     </>
