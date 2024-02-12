35ab2e8a26feb01cf03b4a0fb491624bc91d1d17
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-12 19:55:28 -0300

    se trae los productos de la API en el montaje de Administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,11 +1,25 @@
 import { Table, Container } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
-import { Link } from "react-router-dom";
-
+import { Await, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { leerProductosAPI } from "../../helpers/queries";
 
 const Administrador = () => {
+  const [productos, setProductos] = useState([]);
+  useEffect(() => {
+    traerProductos();
+  }, []);
+
+  const traerProductos = async ()=> {
+    try {
+      await leerProductosAPI()
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <Container className="my-4">
+    <Container className="my-4 mainpage">
       <div className="d-flex justify-content-between align-items-center mt-5">
         <h3 className="display-4">Productos disponibles</h3>
         <Link className="btn btn-primary" to="/administrador/crear">
--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -5,7 +5,9 @@ console.log(URI_PRODUCTOS);
 export const leerProductosAPI = async()=>{
     try{
         const respuesta = await fetch(URI_PRODUCTOS);
+        /* console.log(respuesta); */
         const listaProductos = await respuesta.json();
+        /* console.log(listaProductos); */
         return listaProductos;
     }catch (error){
         console.log(error)
