2644250e53ffd8961a78bf256385495af74f913f
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-11 23:57:36 -0300

    se agrega ruta de inicio a Error404

--- src/components/Common/Menu.jsx
+++ src/components/Common/Menu.jsx
@@ -1,25 +1,39 @@
-import {Navbar, Container, Nav} from 'react-bootstrap'
-import logo from '../../assets/Coffee_Logo.png'
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../../assets/Coffee_Logo.png";
+import { Link, NavLink } from "react-router-dom";
 
 const Menu = () => {
-    return (
-        <Navbar expand="lg" className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="#home">
-            <img src={logo} alt="logo Rolling Coffee" className='img-fluid' width={150}/>
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Nav.Link href="#home">Inicio</Nav.Link>
-              <Nav.Link href="#link">Administrador</Nav.Link>
-              <Nav.Link href="#link">Login</Nav.Link>
-              <Nav.Link href="#link">Registro</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img
+            src={logo}
+            alt="logo Rolling Coffee"
+            className="img-fluid"
+            width={150}
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <NavLink end className="nav-link" to="/">
+              Inicio
+            </NavLink>
+            <NavLink end className="nav-link" to="/administrador">
+              Administrador
+            </NavLink>
+            <NavLink end className="nav-link" to="/login">
+              Login
+            </NavLink>
+            <NavLink end className="nav-link" to="/registro">
+              Registro
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -1,19 +1,22 @@
-import {Container ,Image, Button} from 'react-bootstrap';
-import logoError from '../../../src/assets/error404.png'
+import { Container, Image, Button } from "react-bootstrap";
+import logoError from "../../../src/assets/error404.png";
+import { Link, NavLink } from "react-router-dom";
 
 const Error404 = () => {
-    return (
-        <>
-        <Container> 
-        <div className='text-center d-flex align-items-center justify-content-center flex-column' >
-          <Image  src={logoError} className='img-fluid me-4' />
-          <Button variant='success' className='my-2 py-2 '>
-          Ir al Inicio
-          </Button>
-        </div>
-        </Container>
-          </>
-    );
+  return (
+    <>
+      <Container>
+        <NavLink end className="nav-link" to="/">
+          <div className="text-center d-flex align-items-center justify-content-center flex-column">
+            <Image src={logoError} className="img-fluid me-4" />
+            <Button variant="success" className="my-2 py-2" to="/">
+              Ir al Inicio
+            </Button>
+          </div>
+        </NavLink>
+      </Container>
+    </>
+  );
 };
 
 export default Error404;
