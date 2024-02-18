04c825d6b2196ca841bb7aff0ff3d9118342e532
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-18 10:34:17 -0300

    Agregamos la libreria de sweetalert2 y un mensaje al usuario cuando creamos un producto

--- src/components/pages/producto/FormularioProducto.jsx
+++ src/components/pages/producto/FormularioProducto.jsx
@@ -1,25 +1,34 @@
 import { Form, Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { crearProductoAPI } from "../../../helpers/queries";
+import Swal from "sweetalert2";
 
 const FormularioProducto = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
 
-  const productoValidado = async(producto) => {
+  const productoValidado = async (producto) => {
     console.log(producto);
     const respuesta = await crearProductoAPI(producto);
-    if(respuesta.status === 201){
-    //mensaje para el usuario
-    console.log('producto creado')
-    reset();
-  }else{
-    console.log('ocurrio un error')
-  }
+    if (respuesta.status === 201) {
+      //mensaje para el usuario
+      Swal.fire({
+        title: "Producto creado",
+        text: `El producto: ${producto.nombreProducto} fue creado con exito`,
+        icon: "success",
+      });
+      reset();
+    } else {
+      Swal.fire({
+        title: "Ocurrio un error",
+        text: "Intente crear el producto en unos minutos",
+        icon: "error",
+      });
+    }
   };
 
   return (
@@ -30,87 +39,117 @@ const FormularioProducto = () => {
       <Form className="my-4" onSubmit={handleSubmit(productoValidado)}>
         <Form.Group className="mb-3" controlId="formNombreProdcuto">
           <Form.Label>Producto*</Form.Label>
-          <Form.Control type="text" placeholder="Ej: Cafe" {
-            ...register("nombreProducto",{
-              required:"Este campo es obligatorio", minLength:{value:2,
-              message:"Ingresar como minimo 2 caracteres"},
-              maxLength:{value:40, message:"Ingresar como maximo 40 caracteres"}
-            })
-          } />
-          <Form.Text className="text-danger">{errors.nombreProducto?.message}</Form.Text>
+          <Form.Control
+            type="text"
+            placeholder="Ej: Cafe"
+            {...register("nombreProducto", {
+              required: "Este campo es obligatorio",
+              minLength: {
+                value: 2,
+                message: "Ingresar como minimo 2 caracteres",
+              },
+              maxLength: {
+                value: 40,
+                message: "Ingresar como maximo 40 caracteres",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.nombreProducto?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Precio*</Form.Label>
-          <Form.Control type="number" placeholder="Ej: 50" {
-            ...register("precio",{
-              required:"El precio es obligatorio", min:{value:50,
-              message:"El precio minimo es de $50"},
-              max:{value:10000, message:"El precio maximo es de $10.000"}
-            })
-          } />
-          <Form.Text className="text-danger">{errors.precio?.message}</Form.Text>
+          <Form.Control
+            type="number"
+            placeholder="Ej: 50"
+            {...register("precio", {
+              required: "El precio es obligatorio",
+              min: { value: 50, message: "El precio minimo es de $50" },
+              max: { value: 10000, message: "El precio maximo es de $10.000" },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.precio?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Imagen URL*</Form.Label>
           <Form.Control
-            
             type="text"
             placeholder="Ej: https://www.pexels.com/es-es/vans-en-blanco-y-negro-fuera-de-la-decoracion-para-colgar-en-la-pared-1230679/"
-            {
-              ...register("imagen",{
-                required:"La imagen es obligatoria",
-                pattern:{
-                  value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
-                  message: "Debe ingresar una URL valida (jpg|gif|png)"
-                }
-              })
-            }/>
-          <Form.Text className="text-danger">{errors.imagen?.message}</Form.Text>
+            {...register("imagen", {
+              required: "La imagen es obligatoria",
+              pattern: {
+                value: /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/,
+                message: "Debe ingresar una URL valida (jpg|gif|png)",
+              },
+            })}
+          />
+          <Form.Text className="text-danger">
+            {errors.imagen?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formPrecio">
           <Form.Label>Categoría*</Form.Label>
-          <Form.Select {
-              ...register("categoria",{
-                required:"Debe seleccionar una categoria",
-              })
-            }>
+          <Form.Select
+            {...register("categoria", {
+              required: "Debe seleccionar una categoria",
+            })}
+          >
             <option value="">Seleccione una opcion</option>
             <option value="Infusiones">Infusiones</option>
             <option value="Batidos">Batidos</option>
             <option value="dulce">Dulce</option>
             <option value="salado">Salado</option>
           </Form.Select>
-          <Form.Text className="text-danger">{errors.categoria?.message}</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.categoria?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción breve*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: Una taza de café suave y aromático."
-            as="textarea" {
-              ...register("descripcion_breve",{
-                required:"Este campo es obligatorio", minLength:{value:5,
-                message:"Ingresar como minimo 5 caracteres"},
-                maxLength:{value:40, message:"Ingresar como maximo 40 caracteres"}
-              })
-            }
+            as="textarea"
+            {...register("descripcion_breve", {
+              required: "Este campo es obligatorio",
+              minLength: {
+                value: 5,
+                message: "Ingresar como minimo 5 caracteres",
+              },
+              maxLength: {
+                value: 40,
+                message: "Ingresar como maximo 40 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">{errors.descripcion_breve?.message}</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcion_breve?.message}
+          </Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formImagen">
           <Form.Label>Descripción Amplia*</Form.Label>
           <Form.Control
             type="text"
             placeholder="Ej: El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día."
-            as="textarea" {
-              ...register("descripcion_amplia",{
-                required:"Este campo es obligatorio", minLength:{value:50,
-                message:"Ingresar como minimo 50 caracteres"},
-                maxLength:{value:500, message:"Ingresar como maximo 250 caracteres"}
-              })
-            }
+            as="textarea"
+            {...register("descripcion_amplia", {
+              required: "Este campo es obligatorio",
+              minLength: {
+                value: 50,
+                message: "Ingresar como minimo 50 caracteres",
+              },
+              maxLength: {
+                value: 500,
+                message: "Ingresar como maximo 250 caracteres",
+              },
+            })}
           />
-          <Form.Text className="text-danger">{errors.descripcion_amplia?.message}</Form.Text>
+          <Form.Text className="text-danger">
+            {errors.descripcion_amplia?.message}
+          </Form.Text>
         </Form.Group>
 
         <Button type="submit" variant="success">
