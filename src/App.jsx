f66e2ac7285771e21472b40f907cf0245352f237
Pabmoy25 <pabmoy1725@gmail.com>
2024-02-26 19:54:33 -0300

    se realiza maquetado inicial de componente Login

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Administrador from "./components/pages/Administrador";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DetalleProducto from "./components/pages/DetalleProducto";
 import FormularioProducto from "./components/pages/producto/FormularioProducto";
+import Login from "./components/pages/Login";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
   <Route>
   <Route exact path="/" element={<Inicio></Inicio>}></Route>
   <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+  <Route exact path="/login" element={<Login></Login>}></Route>
   <Route exact path="/detalleproducto/:id" element={<DetalleProducto></DetalleProducto>}></Route>
   <Route exact path="/administrador/crear" element={<FormularioProducto editar={false} titulo='Nuevo producto'></FormularioProducto>}></Route>
   <Route exact path="/administrador/editar/:id" element={<FormularioProducto editar={true} titulo='Editar producto'></FormularioProducto>}></Route>
